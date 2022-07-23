03ee95996a51ac878832bea2dc58f874a4e9d84c
raigormax37417 <raigormax@gmail.com>
2022-07-22 23:05:34 -0500

    agregando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBR1hWzyGyNwkAX1t_KuDZ-rL2q2Zk2CKI",
+    authDomain: "nuevo-ingreso-ee609.firebaseapp.com",
+    projectId: "nuevo-ingreso-ee609",
+    storageBucket: "nuevo-ingreso-ee609.appspot.com",
+    messagingSenderId: "624960709715",
+    appId: "1:624960709715:web:aa584153011490844fd2a6"
+  },
   production: false
 };
 
